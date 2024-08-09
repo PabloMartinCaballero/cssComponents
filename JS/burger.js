7ddcc45ccf03ba13e591b831cbf2c479ca4cc679
PabloDev <Pablo_martin_caballero21@hotmail.com>
2024-08-08 22:43:51 -0300

    Primer menu

---
+++ JS/burger.js
@@ -0,0 +1,12 @@
+const hamburguesa = document.querySelector('.burger-icon');
+console.log(hamburguesa);
+
+const navContainer = document.querySelector('.nav-container')
+console.log(navContainer);
+
+
+hamburguesa.addEventListener('click', ()=>{
+
+    navContainer.classList.toggle('active')
+
+})
